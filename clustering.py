c73086cd25eb58ed7530595721d86ebf7c547f10
Delcatel <philippe.fdesousa@gmail.com>
2017-05-18 11:10:21 +0200

    oba

--- clustering.py
+++ clustering.py
@@ -1,6 +1,17 @@
 from scipy.cluster import *
 # https://docs.scipy.org/doc/scipy/reference/cluster.html
 from generator import *
+from oba import *
 
 def square_kmeans(x1=0, x2=1, y1=0, y2=1, num=100, loi=uniform, k=4):
 	return vq.kmeans(square_data(x1=0, x2=1, y1=0, y2=1, num=100, loi=uniform), k)
+
+def obaTest():
+	set = []
+	for a, b in [(-1, 1), (-1, -1), (1, 1), (1, -1)]:
+		set.extend(square_data(a-0.5, a+0.5, b-0.5, b+0.5, 100))
+
+	# oba(rectToComplex(set))
+	oba(rectToComplex(set), 8, 3)
+
+obaTest()
--- generator.py
+++ generator.py
@@ -3,3 +3,9 @@ from scipy.stats import *
 
 def square_data(x1=0, x2=1, y1=0, y2=1, num=100, loi=uniform):
 	return zip(loi.rvs(loc=x1, scale=x2-x1, size=num), loi.rvs(loc=y1, scale=y2-y1, size=num))
+
+# import matplotlib.pyplot as plt
+# l = square_data(0, 1, 0, 1, 10)
+# 
+# plt.scatter([a for a, b in l], [b for a, b in l])
+# plt.show()
