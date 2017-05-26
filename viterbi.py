4665480491f6658237ad744682189052c7d21c7d
jeanvontrier <jeancv@live.fr>
2017-05-26 13:53:13 +0200

    summary
    l'histo précédent était faux. ceux-ci sont bons, j'ai créé le fichier
    analyse pour les fonctions d'analyse de résultats. j'ai fait de la place
    dans viterbi

--- viterbi.py
+++ viterbi.py
@@ -3,9 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from random import random
-from generator import *
-from oba import *
 from GenereCarre import GenerationCarre2
+from analyse import*
 
 def viterbi(data,H):
     H_estime=0
@@ -29,44 +28,7 @@ def viterbi_test(nbpoints,snr):
     (R,theta,data) = GenerationCarre2(nbpoints,snr)
     H = R*np.exp(complex(0,theta))
     H_estime = viterbi(data,H)
-    return np.absolute((H-H_estime)/H)
-    
-
-def tests_intensifs(eps,nbtests,nbpoints,snr):
-    erreurs = 0
-    for k in range(nbtests):
-        ecart = viterbi_test(nbpoints,snr)
-        if ecart > eps:
-            erreurs +=1
-    return erreurs/nbtests*100
-    
-    
-    
-
-def histoviterbi(nbpoints,snr,nbTests,dispIntervals):
-    Data = []
-    for i in range(1, nbTests+1):
-        if(i%dispIntervals==0):
-            print("Test numéro "+str(i)+" sur "+str(nbTests)+"\n")
-        Data.append(viterbi_test(nbpoints,snr))
-    eff, val, patches = plt.hist(Data, range = (0, 1), bins = 25, edgecolor = 'black', normed=True)
-    plt.xlabel("Erreur sur H (en %)")
-    plt.ylabel("Proportion d'occurrence sur "+str(nbTests)+" tests")
-    plt.title("Viterbi Algorithm")
-    
-    mu = mean(Data)
-    sigma = std(Data)
-    print("mu = "+str(mu))
-    print("std = "+str(sigma))
-    l = max(eff)
-    plt.plot([mu, mu], [0, l], color="red", ls="--")
-    plt.plot([mu-sigma, mu-sigma], [0, l*3/4], color="green", ls=":")
-    plt.plot([mu+sigma, mu+sigma], [0, l*3/4], color="green", ls=":")
-    plt.annotate(r'$\mu$ = '+str(round(mu, 2))+'%', xy=(mu, l), xytext=(mu+0.2, l), color="red")
-    plt.annotate(r'$\sigma$ = '+str(round(sigma, 2))+'%', xy=(mu+sigma, l*3/4), xytext=(mu+sigma+0.2, l*3/4), color="green")
-
-    #plt.savefig("histogramOba"+str(nbTests)+".png")
-    plt.show()
+    return np.absolute((H-H_estime)/H)*100
 
 
         
@@ -82,4 +44,8 @@ def main():
     print(H_estime)
 
 
-histoviterbi(1000,4,1000,1)
+
+#histogram_erreur_relative(1000,1,viterbi_test,10000,10,"Viterbi",(0, 5))
+#(l,taux_cata) = tests_intensifs(5,100,10000,5,viterbi_test)
+#print(l)
+#print(taux_cata)
