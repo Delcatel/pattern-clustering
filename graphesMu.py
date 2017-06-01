483516ee3def2dab1a57864c31a03f80a11a8fa1
Delcatel <philippe.fdesousa@gmail.com>
2017-06-01 11:02:04 +0200

    Ajout graphes pour tracer mu en fonction de threshold pour oda

---
+++ graphesMu.py
@@ -0,0 +1,37 @@
+from matplotlib.pyplot import *
+from numpy import array
+from clustering import *
+
+def muOda():	# N=500, 1000 tests, eps = 0.5 -> snr = 2/0.25=8
+	thresholdOda = array([0.05, 0.1, 0.15, 0.2, 0.3, 0.45, 0.68])
+	muOda = array([3.43, 3.41, 3.44, 3.56, 3.74, 4.05, 4.49])
+	sigmaOda = array([1.05, 1.02, 1.05, 1.09, 1.12, 1.27, 1.6])
+
+	#for k in threshold:
+	#	a, b = histogramOda(1000, 50, k)
+	#	muOda.append(a)
+	#	sigmaOda.append(b)
+
+	plot(thresholdOda, muOda, color="red")
+	plot(thresholdOda, muOda+sigmaOda, color="green")
+	plot(thresholdOda, muOda-sigmaOda, color="green")
+	xlim(0, 0.7)
+	ylim(0, 7)
+	savefig("images/errorOfThresholdOda.png")
+	show()
+
+def muOba():	# N=500, 1000 tests, eps = 0.5 -> snr = 2/0.25=8
+	thresholdOba = []#array([0.05, 0.1, 0.15, 0.2, 0.3, 0.45, 0.68])
+	muOba = []#array([1.49, 1.51, 1.5, 1.46, 1.42, 1.44, 1.42])
+	sigmaOba = []#array([0.82, 0.83, 0.81, 0.78, 0.73, 0.76])
+
+	plot(thresholdOba, muOba, color="red")
+	plot(thresholdOba, muOba+sigmaOba, color="green")
+	plot(thresholdOba, muOba-sigmaOba, color="green")
+	xlim(0, 0.7)
+	ylim(0, 7)
+	savefig("images/errorOfThresholdOba.png")
+	show()
+
+muOda()
+muOba()
